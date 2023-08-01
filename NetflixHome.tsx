051a9a3c441daf9312751f40cdd9d8de1649127f
Uttam25 <131939698+Uttam25@users.noreply.github.com>
2023-08-01 19:39:21 +0530

    file missed

---
+++ NetflixHome.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+function NetflixHome() {
+    return (
+        // <>
+        //     <div className='bg-[url("C:\Users\uttam\Videos\netflix1\my-app\public\netflix-bg.png")] bg-cover p-2 m-2 flex bg-white-100'>
+        //     <div>
+                
+        //     <header className='flex w-full bg-transparent opacity-0.7'>
+        //         <h1 className='w-3/4 ml-7 text-red-500 font-bold text-6xl font-Netflix Sans,Helvetica Neue,Segoe UI,Roboto,Ubuntu,sans-serif'>NETFLIX</h1>
+        //         <select className=' p-2 ml-5 ' id="optionSelect">
+        //             <option lang='en' label='ENGLISH' value="opt1">ENGLISH</option>
+        //             <option lang='hi' label='HINDI' value="opt2">HINDI</option>
+        //         </select>
+        //         <button className='p-3 ml-5 rounded-lg border-2 border-sm border-black text-white bg-red-500'>
+        //             sign in
+        //         </button>
+        //     </header> 
+        //     </div>
+            
+    
+        
+        //     <section className=' block items-end bg-transparent opacity-0.1 text-center  align-bottom'>
+        //         <h1 className='font-white font-bold text-4xl text-white '>Unlimited Movies,TV Shows and more</h1>
+        //         <p className='font-white text-1xl text-white '>Watch anywhere. Cancel anytime.</p>
+        //         <p className='font-white text-1xl text-white '>Ready to watch? Enter your email to create or restart your membership.</p>
+
+        //         <input type="text" placeholder='Email Address' />
+        //         <button className='bg-transparent text-red-500'>Get Started</button>
+
+
+        //     </section>
+        //     </div>
+        // </>
+      <h1>12</h1>
+ )
+  }
+  
+  export default NetflixHome
---
+++ page.tsx
@@ -0,0 +1,47 @@
+import styles from './page.module.css'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    
+      <>
+      <main className={styles.main}>
+        <nav className={styles.nav}>
+          
+            <Image
+              src="/logo.svg"
+              alt="Netflix Logo"
+              className={styles.netflixLogo}
+              width={200}
+              height={100}
+              />
+             <header>
+           <select className={styles.select} id="optionSelect">
+                <option lang='en' label='ENGLISH' value="opt1">ENGLISH</option>
+                <option lang='hi' label='HINDI' value="opt2">HINDI</option>
+              </select>
+              <button className={styles.button}>
+                  sign in
+              </button>
+              </header>
+        </nav>
+          
+          <section className={styles.section}>
+            
+          
+            <h1 className={styles.h1}>Unlimited Movies,TV Shows and more</h1>
+            <p className={styles.p}>Watch anywhere. Cancel anytime.</p>
+            <p className={styles.p}>Ready to watch? Enter your email to create or restart your membership.</p>
+            <div className={styles.div}>
+              <input type="text" placeholder='Email Address' className={styles.input}/>
+              <button className={styles.button}>Get Started</button>
+              </div>
+          </section>
+
+          </main>
+      </>
+  )
+}
+
+
+
