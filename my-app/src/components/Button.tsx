effff3764054e0f013bdad9b81de4fc85ab982a4
Uttam25 <131939698+Uttam25@users.noreply.github.com>
2023-08-01 19:50:15 +0530

    update

---
+++ my-app/src/components/Button.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// const Button = ({children,onClick,className}) => {
+//   return (
+//     <button className='{styles.button}'>
+//         {
+//             children
+//         }
+//     </button>
+//   )
+// }
+
+// export default Button
